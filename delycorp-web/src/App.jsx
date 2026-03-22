2319b859b6bc229a957a73c6807b0be10b22ebd3
Angel <angel.xp.pb@gmail.com>
2026-03-22 17:45:26 -0500

    google analytics

--- delycorp-web/src/App.jsx
+++ delycorp-web/src/App.jsx
@@ -3,6 +3,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CtaSection from "./components/CtaSection";
+import AnalyticsTracker from "./utils/AnalyticsTracker";
 
 import AppRouter from "./router";
 
@@ -11,6 +12,7 @@ import "./App.css";
 function App() {
   return (
     <HashRouter>
+      <AnalyticsTracker />
       <ScrollToTop />
       <div className="app-container">
         <Header />
